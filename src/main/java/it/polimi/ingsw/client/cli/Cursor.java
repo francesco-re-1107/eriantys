37f575b1ee6f1c7441d34ff7bfe7bc756884512a
Rubenxx00 <ruben.scopacasa@gmail.com>
2022-05-03 14:21:30 +0200

    add palette

--- src/main/java/it/polimi/ingsw/client/cli/Cursor.java
+++ src/main/java/it/polimi/ingsw/client/cli/Cursor.java
@@ -8,10 +8,8 @@ import org.fusesource.jansi.AnsiConsole;
 
 import java.util.Collection;
 import java.util.Map;
-import java.util.Scanner;
 
 import static org.fusesource.jansi.Ansi.*;
-import static org.fusesource.jansi.Ansi.Color.*;
 
 
 public class Cursor {
@@ -19,12 +17,12 @@ public class Cursor {
 
     public static final int ISLAND_WIDTH = 11;
     public static final int ISLAND_HEIGHT = 6;
-    private static final Map<Student,Color> STUDENT_COLOR_MAP =  Map.ofEntries(
-            Map.entry(Student.YELLOW, YELLOW),
-            Map.entry(Student.PINK, MAGENTA),
-            Map.entry(Student.RED, RED),
-            Map.entry(Student.BLUE, BLUE),
-            Map.entry(Student.GREEN, GREEN)
+    private static final Map<Student,Integer> STUDENT_COLOR_MAP =  Map.ofEntries(
+            Map.entry(Student.YELLOW, Palette.STUDENT_YELLOW),
+            Map.entry(Student.PINK, Palette.STUDENT_PINK),
+            Map.entry(Student.RED, Palette.STUDENT_RED),
+            Map.entry(Student.BLUE, Palette.STUDENT_BLUE),
+            Map.entry(Student.GREEN, Palette.STUDENT_GREEN)
     );
     final int WIDTH = 80;
     final int HEIGHT = 24;
@@ -38,7 +36,11 @@ public class Cursor {
 
     private Cursor() {
         AnsiConsole.systemInstall();
-        clearScreen();
+        System.setProperty("jansi.passthrough", "true");
+        System.out.print( ansi().eraseScreen());
+        for (int i = 0; i < HEIGHT; i++) {
+            System.out.println(ansi().bg(Palette.ISLAND_BACKGROUND).a(" ".repeat(WIDTH)));
+        }
     }
     public void clearScreen(){
         System.out.print(ansi().eraseScreen().reset());
@@ -57,8 +59,6 @@ public class Cursor {
         }
         moveToXY(1,HEIGHT-1);
         System.out.print('╚');
-
-        new Scanner(System.in).next();
     }
 
     // draws a list of islands
@@ -70,62 +70,77 @@ public class Cursor {
 
         ReducedIsland island = iterator.next();
         for (int i = 0; i < 5; i++) {
-            drawIsland(x_delimiter + ((ISLAND_WIDTH+1)*i), incremental_y, island);
+            drawIsland(x_delimiter + ((ISLAND_WIDTH+1)*i), incremental_y, island, String.valueOf(i));
             island = iterator.next();
         }
         incremental_y += ISLAND_HEIGHT + 1;
 
-        drawIsland(x_delimiter + 4 * (ISLAND_WIDTH + 1), incremental_y, island);
+        drawIsland(x_delimiter + 4 * (ISLAND_WIDTH + 1), incremental_y, island, String.valueOf(5));
         island = iterator.next();
 
         //TODO: fix
         incremental_y += ISLAND_HEIGHT + 1;
-        for (int i = 6; i < count - i; i++) {
-            drawIsland(x_delimiter + ((ISLAND_WIDTH + 1) * i), incremental_y, island);
+        for (int i = 6; i < count - 1; i++) {
+            drawIsland(x_delimiter - ((ISLAND_WIDTH + 1) * i), incremental_y, island, String.valueOf(i));
             island = iterator.next();
         }
         incremental_y -= ISLAND_HEIGHT + 1;
-        drawIsland(x_delimiter, incremental_y, island);
+        drawIsland(x_delimiter, incremental_y, island, String.valueOf(9));
+    }
+
+    private void drawWithBg(String content){
+        System.out.print(ansi().bg(Palette.ISLAND_BACKGROUND).a(content).reset());
     }
 
     // draws and island starting at
-    private void drawIsland(int x0, int y0, ReducedIsland island){
+    private void drawIsland(int x0, int y0, ReducedIsland island, String id){
         moveToXY(x0,y0);
-        System.out.print('╭');
-        System.out.print("─".repeat(ISLAND_WIDTH -2));
-        System.out.print('╮');
+
+        String border = "╭" + "─".repeat(ISLAND_WIDTH -2) + "╮";
+        drawWithBg(border);
 
         moveToXY(x0,y0+1);
-        System.out.print("│ I0");
+        drawWithBg("| " + id);
         moveToXY(x0+ ISLAND_WIDTH -1,y0+1);
-        System.out.print("│");
+        drawWithBg("|");
 
         moveToXY(x0,y0+2);
-        System.out.print("│");
+        drawWithBg("|");
         String s = "";
         for (var x: island.students().getStudents().entrySet()) {
-            s += ansi().fg(STUDENT_COLOR_MAP.get(x.getKey())).a("●".repeat(x.getValue())).reset();
+            s += ansi().bg(Palette.ISLAND_BACKGROUND).fg(STUDENT_COLOR_MAP.get(x.getKey())).a("●".repeat(x.getValue())).reset();
         }
         System.out.println(s);
         moveToXY(x0+ ISLAND_WIDTH -1,y0+2);
-        System.out.print("│");
+        drawWithBg("|");
 
         for (int i = 0; i < ISLAND_HEIGHT - 3; i++) {
             moveToXY(x0,y0+3+i);
-            System.out.println("│" + " ".repeat(ISLAND_WIDTH -2) + "│");
+            drawWithBg("|" + " ".repeat(ISLAND_WIDTH -2) + "|");
         }
 
         moveToXY(x0,y0+ ISLAND_HEIGHT -1);
-        System.out.print("│ ");
-        System.out.println( ansi().fg(island.towerColor() == Tower.BLACK ? BLACK : WHITE).a("♜".repeat(island.towersCount())).reset());
+        drawWithBg("| ");
+
+
+        System.out.println( ansi().fg(getTowerAnsiColor(island.towerColor())).bg(117).a("♜".repeat(island.towersCount())).reset());
         moveToXY(x0+ ISLAND_WIDTH -1,y0+ ISLAND_HEIGHT -1);
-        System.out.print("│");
+        drawWithBg("|");
 
         moveToXY(x0,y0+ ISLAND_HEIGHT);
-        System.out.print('╰');
-        System.out.print("─".repeat(ISLAND_WIDTH -2));
-        System.out.print('╯');
+
+        border = "╰" + "─".repeat(ISLAND_WIDTH -2) + "╯";
+        drawWithBg(border);
     }
+
+    private int getTowerAnsiColor(Tower color){
+        return switch (color){
+            case BLACK -> Palette.TOWER_BLACK;
+            case WHITE -> Palette.TOWER_WHITE;
+            case GREY -> Palette.TOWER_GREY;
+        };
+    }
+
     private void moveToXY(int x, int y){
         System.out.print(ansi().cursor(y, x).reset());
     }
---
+++ src/main/java/it/polimi/ingsw/client/cli/Palette.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.client.cli;
+
+public class Palette {
+    public static final int STUDENT_RED = 9;
+    public static final int STUDENT_GREEN = 36;
+    public static final int STUDENT_YELLOW = 220;
+    public static final int STUDENT_PINK = 169;
+    public static final int STUDENT_BLUE = 39;
+    public static final int ISLAND_BORDER = 22;
+    public static final int ISLAND_BACKGROUND = 117;
+
+    public static final int TOWER_BLACK = 0;
+    public static final int TOWER_WHITE = 15;
+    public static final int TOWER_GREY = 250;
+
+    public static final int MOTHER_NATURE = 130;
+}
