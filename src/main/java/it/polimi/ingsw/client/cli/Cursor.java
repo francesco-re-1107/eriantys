c1179a7721cfdfb997a4dfcda54029e965844afc
Rubenxx00 <ruben.scopacasa@gmail.com>
2022-05-15 11:26:53 +0200

    fixes

--- src/main/java/it/polimi/ingsw/client/cli/Cursor.java
+++ src/main/java/it/polimi/ingsw/client/cli/Cursor.java
@@ -8,69 +8,70 @@ import org.fusesource.jansi.AnsiConsole;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.Scanner;
 
 import static org.fusesource.jansi.Ansi.*;
-
 
 public class Cursor {
     public static Cursor instance = null;
 
     public static final int ISLAND_WIDTH = 11;
     public static final int ISLAND_HEIGHT = 6;
-    private static final Map<Student,Integer> STUDENT_COLOR_MAP =  Map.ofEntries(
+    private static final Map<Student, Integer> STUDENT_COLOR_MAP = Map.ofEntries(
             Map.entry(Student.YELLOW, Palette.STUDENT_YELLOW),
             Map.entry(Student.PINK, Palette.STUDENT_PINK),
             Map.entry(Student.RED, Palette.STUDENT_RED),
             Map.entry(Student.BLUE, Palette.STUDENT_BLUE),
-            Map.entry(Student.GREEN, Palette.STUDENT_GREEN)
-    );
+            Map.entry(Student.GREEN, Palette.STUDENT_GREEN));
     final int WIDTH = 80;
     final int HEIGHT = 24;
 
-    public static Cursor getInstance(){
-       if (instance == null) {
-           instance = new Cursor();
-       }
-       return instance;
+    public static Cursor getInstance() {
+        if (instance == null) {
+            instance = new Cursor();
+        }
+        return instance;
     }
 
     private Cursor() {
         AnsiConsole.systemInstall();
         System.setProperty("jansi.passthrough", "true");
-        System.out.print( ansi().eraseScreen());
+        System.out.print(ansi().eraseScreen());
         for (int i = 0; i < HEIGHT; i++) {
             System.out.println(ansi().bg(Palette.ISLAND_BACKGROUND).a(" ".repeat(WIDTH)));
         }
     }
-    public void clearScreen(){
+
+    public void clearScreen() {
         System.out.print(ansi().eraseScreen().reset());
     }
+
     // unused
-    public void drawEdges(){
-        moveToXY(1,1);
+    public void drawEdges() {
+        moveToXY(0, 1);
         System.out.print('╔');
-        System.out.print("═".repeat(WIDTH-2));
+        System.out.print("═".repeat(WIDTH - 2));
         System.out.print('╗');
-        for (int i = 2; i < HEIGHT-1; i++) {
+        for (int i = 2; i < HEIGHT - 1; i++) {
             moveToXY(i, 1);
             System.out.print("║");
             moveToXY(i, WIDTH);
             System.out.print("║");
         }
-        moveToXY(1,HEIGHT-1);
+        moveToXY(1, HEIGHT - 1);
         System.out.print('╚');
     }
 
     // draws a list of islands
-    public void drawIslands(Collection<ReducedIsland> islandList){
-        int x_delimiter = 20;
-        int incremental_y = 1;
+    public void drawIslands(Collection<ReducedIsland> islandList) {
+        int x_delimiter = 22;
+        int incremental_y = 2;
         int count = islandList.size(); // TODO: rearrange island based on count
         var iterator = islandList.iterator();
 
         ReducedIsland island = iterator.next();
         for (int i = 0; i < 5; i++) {
-            drawIsland(x_delimiter + ((ISLAND_WIDTH+1)*i), incremental_y, island, String.valueOf(i));
+            drawIsland(x_delimiter + ((ISLAND_WIDTH + 1) * i), incremental_y, island, String.valueOf(i));
             island = iterator.next();
         }
         incremental_y += ISLAND_HEIGHT + 1;
@@ -78,93 +79,99 @@ public class Cursor {
         drawIsland(x_delimiter + 4 * (ISLAND_WIDTH + 1), incremental_y, island, String.valueOf(5));
         island = iterator.next();
 
-        //TODO: fix
+        // TODO: fix
         incremental_y += ISLAND_HEIGHT + 1;
         for (int i = 6; i < count - 1; i++) {
-            drawIsland(x_delimiter - ((ISLAND_WIDTH + 1) * i), incremental_y, island, String.valueOf(i));
+            drawIsland(x_delimiter + ((ISLAND_WIDTH + 1) * (count - 2 - i)), incremental_y, island, String.valueOf(i));
             island = iterator.next();
         }
         incremental_y -= ISLAND_HEIGHT + 1;
-        drawIsland(x_delimiter, incremental_y, island, String.valueOf(9));
+        drawIsland(x_delimiter, incremental_y, island, String.valueOf(11));
+
+        moveToXY(1, HEIGHT-1);
+        System.out.print("Riga 1");
+        moveToXY(1, HEIGHT);
+        System.out.print("Riga 2");
+        new Scanner(System.in).next();
     }
 
-    private void drawWithBg(String content){
+    private void drawWithBg(String content) {
         System.out.print(ansi().bg(Palette.ISLAND_BACKGROUND).a(content).reset());
     }
 
     // draws and island starting at
-    private void drawIsland(int x0, int y0, ReducedIsland island, String id){
-        moveToXY(x0,y0);
+    private void drawIsland(int x0, int y0, ReducedIsland island, String id) {
+        moveToXY(x0, y0);
 
-        String border = "╭" + "─".repeat(ISLAND_WIDTH -2) + "╮";
+        String border = "╭" + "─".repeat(ISLAND_WIDTH - 2) + "╮";
         drawWithBg(border);
 
-        moveToXY(x0,y0+1);
-        drawWithBg("| " + id);
-        moveToXY(x0+ ISLAND_WIDTH -1,y0+1);
-        drawWithBg("|");
+        moveToXY(x0, y0 + 1);
+        drawWithBg("│ " + id);
+        moveToXY(x0 + ISLAND_WIDTH - 1, y0 + 1);
+        drawWithBg("│");
 
-        moveToXY(x0,y0+2);
-        drawWithBg("|");
+        moveToXY(x0, y0 + 2);
+        drawWithBg("│");
         String s = "";
-        for (var x: island.students().getStudents().entrySet()) {
-            s += ansi().bg(Palette.ISLAND_BACKGROUND).fg(STUDENT_COLOR_MAP.get(x.getKey())).a("●".repeat(x.getValue())).reset();
+        for (var x : island.students().getStudents().entrySet()) {
+            s += ansi().bg(Palette.ISLAND_BACKGROUND).fg(STUDENT_COLOR_MAP.get(x.getKey())).a("●".repeat(x.getValue()))
+                    .reset();
         }
-        System.out.println(s);
-        moveToXY(x0+ ISLAND_WIDTH -1,y0+2);
-        drawWithBg("|");
+        System.out.print(s);
+        moveToXY(x0 + ISLAND_WIDTH - 1, y0 + 2);
+        drawWithBg("│");
 
         for (int i = 0; i < ISLAND_HEIGHT - 3; i++) {
-            moveToXY(x0,y0+3+i);
-            drawWithBg("|" + " ".repeat(ISLAND_WIDTH -2) + "|");
+            moveToXY(x0, y0 + 3 + i);
+            drawWithBg("│" + " ".repeat(ISLAND_WIDTH - 2) + "│");
         }
 
-        moveToXY(x0,y0+ ISLAND_HEIGHT -1);
-        drawWithBg("| ");
+        moveToXY(x0, y0 + ISLAND_HEIGHT - 1);
+        drawWithBg("│ ");
 
+        System.out.println(
+                ansi().fg(getTowerAnsiColor(island.towerColor())).bg(117).a("♜".repeat(island.towersCount())).reset());
+        moveToXY(x0 + ISLAND_WIDTH - 1, y0 + ISLAND_HEIGHT - 1);
+        drawWithBg("│");
 
-        System.out.println( ansi().fg(getTowerAnsiColor(island.towerColor())).bg(117).a("♜".repeat(island.towersCount())).reset());
-        moveToXY(x0+ ISLAND_WIDTH -1,y0+ ISLAND_HEIGHT -1);
-        drawWithBg("|");
+        moveToXY(x0, y0 + ISLAND_HEIGHT);
 
-        moveToXY(x0,y0+ ISLAND_HEIGHT);
-
-        border = "╰" + "─".repeat(ISLAND_WIDTH -2) + "╯";
+        border = "╰" + "─".repeat(ISLAND_WIDTH - 2) + "╯";
         drawWithBg(border);
     }
 
-    private int getTowerAnsiColor(Tower color){
-        return switch (color){
+    private int getTowerAnsiColor(Tower color) {
+        return switch (color) {
             case BLACK -> Palette.TOWER_BLACK;
             case WHITE -> Palette.TOWER_WHITE;
             case GREY -> Palette.TOWER_GREY;
         };
     }
 
-    private void moveToXY(int x, int y){
+    private void moveToXY(int x, int y) {
         System.out.print(ansi().cursor(y, x).reset());
     }
 
-    public void end(){
+    public void end() {
         AnsiConsole.systemUninstall();
     }
 
-    public void printPrompt(String prompt){
+    public void printPrompt(String prompt) {
         printPrompt(prompt, "");
     }
 
-    public void printPrompt(String prompt, String default_resp){
+    public void printPrompt(String prompt, String default_resp) {
         Ansi s = ansi().bold().a("%s".formatted(prompt)).boldOff();
-        if (!default_resp.isEmpty()){
+        if (!default_resp.isEmpty()) {
             s = s.a(" (default %s)".formatted(default_resp));
         }
         s = s.a(": ").reset();
         System.out.println(s);
     }
 
-    public void printBold(String s){
+    public void printBold(String s) {
         System.out.println(ansi().bold().a(s).reset());
     }
-
 
 }
