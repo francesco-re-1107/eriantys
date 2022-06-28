1002f2b3c4f8553e26e74938a46e7dfc0635857b
Francesco Re <francesco.re1107@gmail.com>
2022-06-28 14:35:54 +0200

    Remove fullscreen in windows

--- src/main/java/it/polimi/ingsw/client/gui/GUI.java
+++ src/main/java/it/polimi/ingsw/client/gui/GUI.java
@@ -1,6 +1,5 @@
 package it.polimi.ingsw.client.gui;
 
-import it.polimi.ingsw.Utils;
 import it.polimi.ingsw.client.Client;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -15,10 +14,7 @@ public class GUI extends Application {
         stage.setMinWidth(1600);
         stage.setMinHeight(900);
 
-        if(Utils.isWindows())
-            stage.setFullScreen(true);
-        else
-            stage.setMaximized(true);
+        stage.setMaximized(true);
 
         stage.setTitle("Eriantys");
 
